a290a5d493ef50de908573d521ad4aa8c928ae4f
Zens <thaobaodh@gmail.com>
2023-11-30 18:06:46 +0700

    commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+const API_URL = environment.API_URL;
+const API_KEY = environment.API_KEY;
 
 @Component({
   selector: 'app-home',
@@ -6,7 +11,29 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  weatherTemp :any
+  todayDate = new Date()
+  cityName = ""
+  weatherIcon :any
+  weatherDetails:any
+  name=""
 
-  constructor() {}
+  constructor(public httpClient:HttpClient) {
+    this.loadData()
+  }
+
+  loadData() {
+    this.httpClient.get(`${API_URL}/weather?q=${this.cityName}&appid=${API_KEY}`).subscribe(results => {
+      console.log(results);
+      this.weatherTemp = (results as any).main;
+      this.name = (results as any).name;
+      console.log(this.weatherTemp);
+      this.weatherDetails = (results as any).weather[0];
+      console.log(this.weatherDetails);
+      this.weatherIcon =`https://openweathermap.org/img/wn/${this.weatherDetails.icon}@4x.png` 
+      
+      
+    })
+  }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  API_KEY : '2baf2d55dba7ee9514c24559783adc3a',
+  API_URL : 'https://api.openweathermap.org/data/2.5'
 };
 
 /*
